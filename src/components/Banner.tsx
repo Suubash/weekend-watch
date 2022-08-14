e6e13c1570ba4efba8d82abbe3701fe574bc5424
subash <imsuubash@gmail.com>
2022-08-14 07:21:04 +0545

    add swiper carousel

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,20 +3,47 @@ import styles from "../styles/banner.module.css";
 import { useNavigate } from "react-router-dom";
 import MovieContext from "../contexts/MovieContext";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../styles/Swiper.css";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+
+SwiperCore.use([Autoplay]);
+
 function Banner() {
   const navigate = useNavigate();
   const { movieState } = React.useContext(MovieContext);
   const { topTrendings } = movieState;
+
+  // w440_and_h660_face => low quality poster images
   return (
     <div className={styles.banner}>
-      {topTrendings.length > 1 && (
-        <img
-          src={`https://image.tmdb.org/t/p/w440_and_h660_face${
-            topTrendings[0]?.backdrop_path || topTrendings[0]?.poster_path
-          }`}
-          alt={topTrendings[0]?.title}
-        />
-      )}
+      <div className="swiper-container">
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          freeMode={true}
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          pagination={true}
+          modules={[Pagination]}
+          className="my-swiper"
+        >
+          {topTrendings.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <div className="slides">
+                <img
+                  className="slide-image"
+                  src={`https://image.tmdb.org/t/p/w1280_and_h720_face${
+                    movie.backdrop_path || movie.poster_path
+                  }`}
+                  alt={movie.title || movie.name || movie.original_name}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
       <div className={styles.bannerContentContainer}>
         <div className={styles.bannerContent}>
